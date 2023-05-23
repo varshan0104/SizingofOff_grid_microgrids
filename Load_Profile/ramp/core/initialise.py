e264ebf731e95412280d060668744a2ed025befb
varshan0104 <varshan0104@gmail.com>
2023-05-23 15:03:29 +0200

    Flyttet excel fil for app og user, også lagt inn ny kode for innhenting derfra istedenfor fra fil user_input_1.py

--- Load_Profile/ramp/core/initialise.py
+++ Load_Profile/ramp/core/initialise.py
@@ -4,7 +4,7 @@
 
 import numpy as np 
 import importlib
-
+import pandas as pd
 
 def yearly_pattern():
     '''
@@ -17,17 +17,80 @@ def yearly_pattern():
     
     return(Year_behaviour)
 
+# def user_defined_inputs(j):
+#     file_module = importlib.import_module(f'ramp.input_files.input_file_{j}')
+#     User_list = file_module.User_list
+#     return(User_list)
+
+class User:
+    def __init__(self, name, n_users, us_pref):
+        self.name = name
+        self.n_users = n_users
+        self.us_pref = us_pref
+        self.App_list = []
+
+class Appliance:
+    def __init__(self, user, number, P, num_windows, func_time, r_t, func_cycle, fixed, fixed_cycle, occasional_use, flat, thermal_P_var, pref_index, wd_we_type, year_min, initial_share):
+        self.user = user
+        self.number = number
+        self.P = P
+        self.num_windows = num_windows
+        self.func_time = func_time
+        self.r_t = r_t
+        self.func_cycle = func_cycle
+        self.fixed = fixed
+        self.fixed_cycle = fixed_cycle
+        self.occasional_use = occasional_use
+        self.flat = flat
+        self.thermal_P_var = thermal_P_var
+        self.pref_index = pref_index
+        self.wd_we_type = wd_we_type
+        self.year_min = year_min
+        self.initial_share = initial_share
+
+    def windows(self, time_window_params, r_w):
+        # Define this method based on your requirements
+        pass
+
+
 
 def user_defined_inputs(j):
-    '''
-    Imports an input file and returns a processed User_list
-    '''
+    # Read the Excel file
+    df = pd.read_excel('Load_Profile/ramp/core/Appliances_and_users.xlsx', sheet_name=1)  # Leser første ark (indeks 0)
 
-    file_module = importlib.import_module(f'ramp.input_files.input_file_{j}')
-    
-    User_list = file_module.User_list
+    # Initialize an empty list to hold all users
+    all_users = []
 
-    return(User_list)
+    # Iterate over each row in the DataFrame
+    for index, row in df.iterrows():
+        # Check if the User column is not NaN (i.e., there is a user for this row)
+        if pd.notna(row['User']):
+            # Create a new user
+            user = User(row['User'], row['n_user'], row['us-pref'])
+            all_users.append(user)
+
+        # Create a new appliance
+        appliance = Appliance(user, row['number'], row['P'], row['num-windows'], row['func-time'], row['r_t'], row['func_cycle'], row['fixed'], row['fixed_cycle'], row['occasional_use'], row['flat'], row['thermal_P_var'], row['pref_index'], row['wd_we_type'], row['year_min'], row['initial_share'])
+
+        # Add the appliance to the current user and any other users specified in the 'Connected user' column
+        connected_users = row['Connected user'].split(',')
+        for user_name in connected_users:
+            # Find the user with this name
+            for user in all_users:
+                if user.name == user_name:
+                    # Add the appliance to this user
+                    user.App_list.append(appliance)
+
+        # Parse the time_window string to get the parameters for the .windows() function
+        time_window_str = row['time_window']
+        time_window_params = time_window_str.strip('[]').split(',')
+        time_window_params = [int(param.strip()) for param in time_window_params]
+        r_w = float(time_window_str.split('=')[1])
+
+        # Call the .windows() function on the appliance
+        appliance.windows(time_window_params, r_w=r_w)
+
+    return all_users
 
 
 def Initialise_model(num_profiles):
