4f9831d6fb2616ff82c8aab8570be0d1bf76f35b
varshan0104 <varshan0104@gmail.com>
2023-05-24 20:42:44 +0200

    Resatt all RAMP kode til gammel fil men implementert koden til jentene i input 1

--- Load_Profile/ramp/input_files/input_file_1.py
+++ Load_Profile/ramp/input_files/input_file_1.py
@@ -10,41 +10,43 @@ from Load_Profile.ramp.core.core import User
 
 from Load_Profile.ramp.core.core import User
 
-# Create an instance of User
-user_instance = User(name="user1", n_users=1, us_pref=0)
 
-# Now you can access Appliance through the user_instance
-appliance_instance = user_instance.Appliance(user="user1", n=1, P=0, w=1, t=0, r_t=0, c=1, fixed='no', fixed_cycle=0, occasional_use=1, flat='no', thermal_P_var=0, pref_index=0, wd_we_type=2, P_series=False)
+User_list = []
+
+"""------------- BASE LOAD STAFF COURT -------------"""
+
+"""-------------Hot season; January, 31 days (regular school, high visitor activity)-------------"""
+
+#Staff Court
+SC = User("Staff Court",1,3)
+User_list.append(SC)
 
 
-def load_from_excel(file_path):
-    df = pd.read_excel(file_path, sheet_name='Spring')
-    users = []
-    appliances = []
-    for _, row in df.iterrows():
-        user = User(row['User Name'], row['Number of Users'], row['User Preference'])
-        users.append(user)
-        appliance = user.Appliance(
-            user,
-            row['Number of Appliances'],
-            row['Power of Appliance'],
-            row['Number of Functioning Windows'],
-            row['Function Time'],
-            row['Random Variability Percentage'],
-            row['Function Cycle'],
-            row['Fixed'],
-            row['Fixed Cycle'],
-            row['Occasional Use'],
-            row['Flat'],
-            row['Thermal Power Variation'],
-            row['Preference Index'],
-            row['Weekday/Weekend Type'],
-            row['Year Minimum'],
-            row['Initial Share']
-        )
-        appliances.append(appliance)
-    return users, appliances
 
-users, appliances = load_from_excel('C:/Users/varsh/PycharmProjects/SizingOffGridMicrogrids/Load_Profile/ramp/input_files/Appliances_and_users.xlsx')
 
-User_list = users
+
+"""-------- LIGHTNING --------"""
+
+Indoor_LED_SC_DAY_wd = SC.Appliance(SC,12,9,1,1*60,0,1*60, wd_we_type = 0)
+#time window: 06:00-07:00
+Indoor_LED_SC_DAY_wd.windows([6*60,7*60])
+
+Indoor_LED_SC_DAY_we = SC.Appliance(SC,6,9,1,1*60,0,1*60, wd_we_type = 1)
+#time window: 06:00-07:00
+Indoor_LED_SC_DAY_we.windows([6*60,7*60])
+
+Indoor_LED_SC_NIGHT_wd = SC.Appliance(SC,12,9,1,5*60,0,5*60, wd_we_type = 0)
+#time window: 18:00-23:59
+Indoor_LED_SC_NIGHT_wd.windows([18*60,23*60+59])
+
+Indoor_LED_SC_NIGHT_we = SC.Appliance(SC,6,9,1,5*60,0,5*60, wd_we_type = 0)
+#time window: 18:00-23:59
+Indoor_LED_SC_NIGHT_we.windows([18*60,23*60+59])
+
+Outdoor_LED_SC_DAY = SC.Appliance(SC,6,9,1,1*60,0,1*60)
+#time window: 06:00-07:00
+Outdoor_LED_SC_DAY.windows([6*60,7*60])
+
+Outdoor_LED_SC_NIGHT = SC.Appliance(SC,6,9,1,5*60,0,5*60)
+#time window: 18:00-23:59
+Outdoor_LED_SC_NIGHT.windows([18*60,23*60+59])
